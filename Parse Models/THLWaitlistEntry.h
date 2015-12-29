9c99e41956a3748a45af6cb3149d5f788a55571a
Philip Meyers IV <philipmeyers2017@u.northwestern.edu>
2015-12-29 15:54:53 -0500

    Created WaitlistEntry model

---
+++ Parse Models/THLWaitlistEntry.h
@@ -0,0 +1,15 @@
+//
+//  THLWaitlistEntry.h
+//  Hype
+//
+//  Created by Phil Meyers IV on 12/29/15.
+//  Copyright © 2015 Hypelist. All rights reserved.
+//
+
+#import <Parse/Parse.h>
+#import "PFObject+Subclass.h"
+
+@interface THLWaitlistEntry : PFObject<PFSubclassing>
+@property (nonatomic, retain) NSString *email;
+@property (nonatomic, retain) NSString *code;
+@end
---
+++ Parse Models/THLWaitlistEntry.m
@@ -0,0 +1,22 @@
+//
+//  THLWaitlistEntry.m
+//  Hype
+//
+//  Created by Phil Meyers IV on 12/29/15.
+//  Copyright © 2015 Hypelist. All rights reserved.
+//
+
+#import "THLWaitlistEntry.h"
+
+@implementation THLWaitlistEntry
+@dynamic email;
+@dynamic code;
+
++ (void)load {
+	[self registerSubclass];
+}
+
++ (NSString *)parseClassName {
+	return @"WaitlistEntry";
+}
+@end
