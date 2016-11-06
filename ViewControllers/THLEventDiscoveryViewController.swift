8bbda456f7c0ec2035ba06b9f63dc673b16003cf
Edgar Li <liedgar7@gmail.com>
2016-11-06 13:29:30 -0500

    Added date section headers to event discovery

--- ViewControllers/THLEventDiscoveryViewController.swift
+++ ViewControllers/THLEventDiscoveryViewController.swift
@@ -16,10 +16,44 @@ import BoltsSwift
     func eventDiscoveryViewControllerWantsToPresentDetailsForAttendingEvent(_ event: PFObject, venue: PFObject, invite: PFObject)
 }
 
+class SimpleCollectionReusableView : UICollectionReusableView {
+    let titleLabel: UILabel = UILabel(frame: CGRect.zero)
+    let subtitleLabel: UILabel = UILabel(frame: CGRect.zero)
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        titleLabel.textAlignment = .left
+        titleLabel.textColor = UIColor.white
+        titleLabel.font = UIFont(name: "Raleway-ExtraBold", size: 14)
+        subtitleLabel.textAlignment = .left
+        subtitleLabel.textColor = UIColor.white
+        subtitleLabel.font = UIFont(name: "Raleway-Regular", size: 14)
+
+        addSubview(titleLabel)
+        addSubview(subtitleLabel)
+
+    }
+    
+    required init?(coder decoder: NSCoder) {
+        super.init(coder: decoder)
+
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        titleLabel.frame = CGRect(x:10,y:5,width:frame.size.width,height:20)
+        subtitleLabel.frame = CGRect(x:10,y:25,width:frame.size.width,height:20)
+    }
+}
+
 class THLEventDiscoveryViewController: PFQueryCollectionViewController {
     
     // MARK: Init
     var delegate: THLEventDiscoveryViewControllerDelegate?
+    var sections: [Int: [PFObject]] = Dictionary()
+    var sectionKeys: [Int] = Array()
 
     convenience init(className: String?) {
         let layout = UICollectionViewFlowLayout()
@@ -36,13 +70,16 @@ class THLEventDiscoveryViewController: PFQueryCollectionViewController {
     override func loadView() {
         super.loadView()
         
-        collectionView?.register(THLEventDiscoveryCell.self, forCellWithReuseIdentifier: "THLEventDiscoveryCell")
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         collectionView?.backgroundColor = UIColor.black
         automaticallyAdjustsScrollViewInsets = true
+        
+        collectionView?.register(THLEventDiscoveryCell.self, forCellWithReuseIdentifier: "THLEventDiscoveryCell")
+        collectionView?.register(SimpleCollectionReusableView.self, forSupplementaryViewOfKind: UICollectionElementKindSectionHeader, withReuseIdentifier: "header")
+
     }
     
     override func viewWillLayoutSubviews() {
@@ -70,15 +107,54 @@ class THLEventDiscoveryViewController: PFQueryCollectionViewController {
         
         return query
     }
+    
+    // MARK: Data
+    override func objectsDidLoad(_ error: Error?) {
+        super.objectsDidLoad(error)
+        
+        sections.removeAll(keepingCapacity: false)
+        if let objects = objects as? [PFObject] {
+            for object in objects {
+                let date = (object["date"] as? Date)
+                let day = Calendar.current.component(.day, from: date!)
+                var array = sections[day] ?? Array()
+                array.append(object)
+                sections[day] = array
+            }
+        }
+        sectionKeys = sections.keys.sorted(by: <)
+        
+        collectionView?.reloadData()
+    }
+
+    override func object(at indexPath: IndexPath?) -> PFObject? {
+        if let indexPath = indexPath {
+            let array = sections[sectionKeys[indexPath.section]]
+            return array?[indexPath.row]
+        }
+        return nil
+    }
+
 }
 
 
 extension THLEventDiscoveryViewController {
 
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return sections.count
+    }
+    
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        let array = sections[sectionKeys[section]]
+        return array?.count ?? 0
+    }
+
+    
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath, object: PFObject?) -> THLEventDiscoveryCell? {
         let cell:THLEventDiscoveryCell = collectionView.dequeueReusableCell(withReuseIdentifier: "THLEventDiscoveryCell", for: indexPath) as! THLEventDiscoveryCell
         
-        var event:THLEvent? = object as? THLEvent
+        let event:THLEvent? = object as? THLEvent
         cell.venueImageView.file = event?.location?.image
         cell.venueImageView.loadInBackground()
         cell.venueNameLabel.text = event?.location?.name.uppercased()
@@ -116,5 +192,27 @@ extension THLEventDiscoveryViewController {
             self.delegate?.eventDiscoveryViewControllerWantsToPresentDetailsForEvent(event!, venue: event?.value(forKey: "location") as! PFObject)
         }
         
+    }
+    
+
+    override func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        if kind == UICollectionElementKindSectionHeader,
+            let view = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: "header", for: indexPath) as? SimpleCollectionReusableView {
+            let event = object(at: indexPath) as? PFObject!
+            let date:NSDate = event?.value(forKey:"date") as! NSDate
+            
+            view.titleLabel.text = date.thl_dayOfTheWeek().uppercased()
+            view.subtitleLabel.text = date.thl_dateString.uppercased()
+            return view
+        }
+        return super.collectionView(collectionView, viewForSupplementaryElementOfKind: kind, at: indexPath)
+    }
+
+    
+    override func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
+        if sections.count > 0 {
+            return CGSize(width: collectionView.bounds.width, height: 50.0)
+        }
+        return CGSize.zero
     }
 }
--- Views/THLDiscoveryNavBarTitleView.swift
+++ Views/THLDiscoveryNavBarTitleView.swift
@@ -18,10 +18,34 @@ open class THLDiscoveryNavBarTitleView: UIView {
     }
     
     override init(frame:CGRect) {
-        eventButton = UIButton.init(frame: CGRect(x: 0, y: 0, width: 70, height: 20))
-        venueButton = UIButton.init(frame: CGRect(x: 70, y: 0, width: 70, height: 20))
 
-        super.init(frame: CGRect(x: 0,y: 0,width: 140,height: 20))
+        eventButton = UIButton.init(frame: CGRect.zero)
+        venueButton = UIButton.init(frame: CGRect.zero)
+        
+        super.init(frame: CGRect(x: 0,y: 0,width: 200,height: 20))
+
+        
+        eventButton.setTitle("EVENTS", for: UIControlState())
+        eventButton.setTitleColor(UIColor.customGoldColor(), for: UIControlState.selected)
+        eventButton.setTitleColor(UIColor.gray, for: UIControlState())
+        
+        eventButton.titleLabel!.font = UIFont.init(name: "Raleway-Bold", size: 20.0)
+        eventButton.titleLabel!.textAlignment = .left
+        eventButton.isSelected = true
+        
+        venueButton.setTitle("VENUES", for: UIControlState())
+        venueButton.setTitleColor(UIColor.customGoldColor(), for: UIControlState.selected)
+        venueButton.setTitleColor(UIColor.gray, for: UIControlState())
+        
+        venueButton.titleLabel!.font = UIFont.init(name: "Raleway-Bold", size: 20.0)
+        venueButton.titleLabel!.textAlignment = .left
+        venueButton.isSelected = false
+        
+        self.addSubview(eventButton)
+        self.addSubview(venueButton)
+        
+
+
     }
     
     // MARK:- UIView
@@ -29,23 +53,7 @@ open class THLDiscoveryNavBarTitleView: UIView {
     override open func layoutSubviews() {
         super.layoutSubviews()
         
-        eventButton.setTitle("EVENTS", for: UIControlState())
-        eventButton.setTitleColor(UIColor.customGoldColor(), for: UIControlState.selected)
-        eventButton.setTitleColor(UIColor.gray, for: UIControlState())
-
-        eventButton.titleLabel!.font = UIFont.init(name: "OpenSans-Semibold", size: 14.0)
-        eventButton.titleLabel!.textAlignment = .center
-        eventButton.isSelected = true
-        
-        venueButton.setTitle("VENUES", for: UIControlState())
-        venueButton.setTitleColor(UIColor.customGoldColor(), for: UIControlState.selected)
-        venueButton.setTitleColor(UIColor.gray, for: UIControlState())
-
-        venueButton.titleLabel!.font = UIFont.init(name: "Raleway-Bold", size: 14.0)
-        venueButton.titleLabel!.textAlignment = .center
-        venueButton.isSelected = false
-
-        self.addSubview(eventButton)
-        self.addSubview(venueButton)
+        eventButton.frame = CGRect(x: 0, y: 0, width: 100, height: 20)
+        venueButton.frame = CGRect(x: 100, y: 0, width: 100, height: 20)
     }
 }
